38eaacc46e9f2d06e85a2a1fde77c5d09769034c
Robin Sveningson <robin@ourstudio.se>
2018-06-28 14:52:47 +0200

    Created base for api controllers.

---
+++ EmailService/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.Controllers
+{
+	[Route("api/email")]
+	[ApiController]
+	public class EmailController : Controller
+	{
+		// GET api/email
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			return new OkResult();
+		}
+	}
+}
---
+++ EmailService/Controllers/EmailSendController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.Controllers
+{
+    [Route("api/email/send")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST api/email/send
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
